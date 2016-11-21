689d9506e3fe1bd104f2fb51e1dc12a58dd9b0fd
David Lechner <david@lechnology.com>
2016-11-20 22:14:09 -0600

    ARM: davinci: Allocate extra interrupts
    This allocates extra interrupts for mach-davinci. These extra interrupts
    are need for things like IIO triggers.
    
    Signed-off-by: David Lechner <david@lechnology.com>

--- arch/arm/mach-davinci/include/mach/irqs.h
+++ arch/arm/mach-davinci/include/mach/irqs.h
@@ -403,8 +403,10 @@
 
 /* da850 currently has the most gpio pins (144) */
 #define DAVINCI_N_GPIO			144
+/* Extra IRQs for things like IIO triggers */
+#define DAVINCI_N_SPARE_IRQ		16
 /* da850 currently has the most irqs so use DA850_N_CP_INTC_IRQ */
-#define NR_IRQS				(DA850_N_CP_INTC_IRQ + DAVINCI_N_GPIO)
+#define NR_IRQS (DA850_N_CP_INTC_IRQ + DAVINCI_N_GPIO + DAVINCI_N_SPARE_IRQ)
 
 /*
  * This value is needed by arch/arm/kernel/fiq.c but is not meaningful
