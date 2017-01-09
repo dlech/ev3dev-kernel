c475c14614b3328d73252d9352f65e6a727a7fbe
Alexandre Bailon <abailon@baylibre.com>
2017-01-09 17:39:02 +0100

    usb: musb: musb_cppi41: Workaround dma stall issue during teardown
    The dma may hung up if a teardown is initiated while an endpoint is still
    active (Advisory 2.3.27 of da8xx errata).
    To workaround this issue, add a delay before to initiate the teardown.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

--- drivers/usb/musb/da8xx.c
+++ drivers/usb/musb/da8xx.c
@@ -458,7 +458,7 @@ static inline u8 get_vbus_power(struct device *dev)
 }
 
 static const struct musb_platform_ops da8xx_ops = {
-	.quirks		= MUSB_DMA_CPPI41 | MUSB_INDEXED_EP,
+	.quirks		= MUSB_DMA_CPPI41 | MUSB_INDEXED_EP | MUSB_DA8XX,
 	.init		= da8xx_musb_init,
 	.exit		= da8xx_musb_exit,
 
