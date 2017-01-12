c6dad9892cbb0c7a6ac56c57f91b85980d4af43b
David Lechner <david@lechnology.com>
2017-01-12 12:55:35 -0600

    mtd: block: use kmalloc instead of vmalloc
    This memory can be passed to DMA, so it can't be vmalloced.

--- drivers/mtd/mtdblock.c
+++ drivers/mtd/mtdblock.c
@@ -267,7 +267,8 @@ static int mtdblock_writesect(struct mtd_blktrans_dev *dev,
 {
 	struct mtdblk_dev *mtdblk = container_of(dev, struct mtdblk_dev, mbd);
 	if (unlikely(!mtdblk->cache_data && mtdblk->cache_size)) {
-		mtdblk->cache_data = vmalloc(mtdblk->mbd.mtd->erasesize);
+		mtdblk->cache_data = kmalloc(mtdblk->mbd.mtd->erasesize,
+					     GFP_KERNEL);
 		if (!mtdblk->cache_data)
 			return -EINTR;
 		/* -EINTR is not really correct, but it is the best match
@@ -320,7 +321,7 @@ static void mtdblock_release(struct mtd_blktrans_dev *mbd)
 		 */
 		if (mbd->file_mode & FMODE_WRITE)
 			mtd_sync(mbd->mtd);
-		vfree(mtdblk->cache_data);
+		kfree(mtdblk->cache_data);
 	}
 
 	pr_debug("ok\n");
