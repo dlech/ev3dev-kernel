2976d45244abdafe5317417fbe94216ed8d4f8f5
David Lechner <david@lechnology.com>
2018-01-06 22:05:05 -0600

    clk: davinci: Add platform information for TI DM365 PSC
    This adds platform-specific declarations for the PSC clocks on TI
    DM365 based systems.
    
    Signed-off-by: David Lechner <david@lechnology.com>
    Reviewed-by: Sekhar Nori <nsekhar@ti.com>

--- drivers/clk/davinci/psc.c
+++ drivers/clk/davinci/psc.c
@@ -450,6 +450,7 @@ static const struct platform_device_id davinci_psc_id_table[] = {
 	{ .name = "da850-psc0", .driver_data = (kernel_ulong_t)da850_psc0_init },
 	{ .name = "da850-psc1", .driver_data = (kernel_ulong_t)da850_psc1_init },
 	{ .name = "dm355-psc",  .driver_data = (kernel_ulong_t)dm355_psc_init  },
+	{ .name = "dm365-psc",  .driver_data = (kernel_ulong_t)dm365_psc_init  },
 	{ }
 };
 
