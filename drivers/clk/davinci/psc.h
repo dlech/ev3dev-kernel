cb93786c45f9b5652e2ddc790cddfe3d16eaec05
David Lechner <david@lechnology.com>
2018-01-06 22:05:05 -0600

    clk: davinci: Add platform information for TI DM644x PSC
    This adds platform-specific declarations for the PSC clocks on TI
    DM644x based systems.
    
    Signed-off-by: David Lechner <david@lechnology.com>

--- drivers/clk/davinci/psc.h
+++ drivers/clk/davinci/psc.h
@@ -95,5 +95,6 @@ int of_da850_psc0_init(struct device *dev, void __iomem *base);
 int of_da850_psc1_init(struct device *dev, void __iomem *base);
 int dm355_psc_init(struct device *dev, void __iomem *base);
 int dm365_psc_init(struct device *dev, void __iomem *base);
+int dm644x_psc_init(struct device *dev, void __iomem *base);
 
 #endif /* __CLK_DAVINCI_PSC_H__ */
