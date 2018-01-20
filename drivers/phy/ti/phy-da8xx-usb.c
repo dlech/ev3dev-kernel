6903bb4fa0d1637f589da514dda5d708ac4f58f1
David Lechner <david@lechnology.com>
2018-01-19 21:03:03 -0600

    phy: da8xx-usb: drop use of syscon_regmap_lookup_by_pdevname()
    This drops the use of syscon_regmap_lookup_by_pdevname() from the
    DA8XX USB PHY driver. There are no longer any boards that register
    a syscon platform device, so it is no longer needed.
    
    Signed-off-by: David Lechner <david@lechnology.com>

--- drivers/phy/ti/phy-da8xx-usb.c
+++ drivers/phy/ti/phy-da8xx-usb.c
@@ -153,8 +153,6 @@ static int da8xx_usb_phy_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	d_phy->regmap = syscon_regmap_lookup_by_compatible("ti,da830-cfgchip");
-	if (IS_ERR(d_phy->regmap))
-		d_phy->regmap = syscon_regmap_lookup_by_pdevname("syscon");
 	if (IS_ERR(d_phy->regmap)) {
 		dev_err(dev, "Failed to get syscon\n");
 		return PTR_ERR(d_phy->regmap);
